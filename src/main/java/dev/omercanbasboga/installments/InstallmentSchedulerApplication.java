c505aca9fd477abd63504eeada939a942f7e410b
Omercanbasboga <94013047+Omercanbasboga@users.noreply.github.com>
2026-09-14 10:31:55 +0300

    Add files via upload

---
+++ src/main/java/dev/omercanbasboga/installments/InstallmentSchedulerApplication.java
@@ -0,0 +1,11 @@
+package dev.omercanbasboga.installments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstallmentSchedulerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InstallmentSchedulerApplication.class, args);
+    }
+}
